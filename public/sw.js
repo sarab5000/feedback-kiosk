7f416192d97a282bdccc96b705a43e8f5155eca1
sarab5000 <32688470+sarab5000@users.noreply.github.com>
2022-02-21 20:04:23 +0200

    styling survey

--- public/sw.js
+++ public/sw.js
@@ -1,45 +1,52 @@
-importScripts('/cache-polyfill.js');
+/*
 
-const cacheName = 'jwkiosk';
+It is stopped for now.
+To be activated later.
 
-self.addEventListener('install', function (e) {
-    console.log('registering the service worker');
+*/
 
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll([
-                '/',
-                '/imgs/logo2.png',
-                '/survey/happy/',
-                '/css/bootstrap-rtl.min.css'
-            ]);
-        })
-    );
-});
+// importScripts('/cache-polyfill.js');
 
-self.addEventListener('fetch', function (event) {
-    console.log(event.request.url);
+// const cacheName = 'jwkiosk';
 
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('install', function (e) {
+//     console.log('registering the service worker');
 
-self.addEventListener('activate', function(event) {
+//     e.waitUntil(
+//         caches.open(cacheName).then(function (cache) {
+//             return cache.addAll([
+//                 '/',
+//                 '/imgs/logo2.png',
+//                 '/survey/happy/',
+//                 '/css/bootstrap-rtl.min.css'
+//             ]);
+//         })
+//     );
+// });
 
-    console.log('Service Worker: Activating....');
+// self.addEventListener('fetch', function (event) {
+//     console.log(event.request.url);
 
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(key) {
-                if( key !== cacheName) {
-                    console.log('Service Worker: Removing Old Cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
-});
+//     event.respondWith(
+//         caches.match(event.request).then(function (response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
+
+// self.addEventListener('activate', function(event) {
+
+//     console.log('Service Worker: Activating....');
+
+//     event.waitUntil(
+//         caches.keys().then(function(cacheNames) {
+//             return Promise.all(cacheNames.map(function(key) {
+//                 if( key !== cacheName) {
+//                     console.log('Service Worker: Removing Old Cache', key);
+//                     return caches.delete(key);
+//                 }
+//             }));
+//         })
+//     );
+//     return self.clients.claim();
+// });
